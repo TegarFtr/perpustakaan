a4daa72fc605a7c81b3b5bd5ae8b88402d5db221
rivanadik <118202475+rivanadik@users.noreply.github.com>
2023-11-26 17:19:22 +0700

    Update penerbit.blade.php

--- resources/views/admin/mainmenu/katalog/penerbit.blade.php
+++ resources/views/admin/mainmenu/katalog/penerbit.blade.php
@@ -4,5 +4,28 @@
 @section('activePenerbit', 'active')
 
 @section('content')
-    <h1>Menu Penerbit</h1>
+<h1 class="m-3">Menu Penerbit</h1>
+<div class="m-4">
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>Nama Penerbit</th>
+                <th>Aksi</th>
+            </tr>
+        </thead>
+        <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>KIWKIW</td>
+                    <td>
+                        <a href="#" class="btn btn-primary mr-2">Edit</a>
+                        <a href="#" class="btn btn-danger">Hapus</a>
+                    </td>
+                </tr>
+        </tbody>
+    </table>
+
+    <a href="#" class="btn btn-primary">Tambah Penerbit</a>
+</div>
 @endsection
