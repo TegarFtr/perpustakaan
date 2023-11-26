fd661e5280f2c5acf503a689de12bbe8ccf59136
TegarFtr <tegar6772@gmail.com>
2023-11-26 22:34:43 +0700

    lanjut

--- resources/views/admin/mainmenu/katalog/kategoriBuku.blade.php
+++ resources/views/admin/mainmenu/katalog/kategoriBuku.blade.php
@@ -1,10 +1,9 @@
 @extends('layouts.master')
 @section('menuKatalog', 'menu-open')
 @section('activeKatalog', 'active')
-@section('activeKategoriBuku', 'active') 
+@section('activeKategoriBuku', 'active')
 
 @section('content')
-<<<<<<< HEAD
     <div class="container-fluid ">
         <div class="row">
             <div class="col-12">
@@ -137,34 +136,4 @@
             </div>
         </div>
     </div>
-=======
-<div class="d-flex justify-content-between">
-    <h1 class="m-3">Menu Kategori Buku</h1> 
-    <div class="mr-5 mt-4">
-        <a href="#" class="btn btn-primary">Tambah Kategori Buku</a>
-    </div>
-</div>
-
-<div class="m-4">
-    <table class="table table-bordered">
-        <thead>
-            <tr>
-                <th>No</th>
-                <th>Nama Kategori</th> 
-                <th>Aksi</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>1</td>
-                <td>Novel</td> 
-                <td>
-                    <a href="#" class="btn btn-primary mr-2">Edit</a>
-                    <a href="#" class="btn btn-danger">Hapus</a>
-                </td>
-            </tr>
-        </tbody>
-    </table>
-</div>
->>>>>>> 0dcab1965e8b2cfe7ab3a35557124848e50a2fa6
 @endsection
